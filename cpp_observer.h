9316580a73dc062d7cc2c43f5f8f98c51c587a24
alex <u@ubuntu-14.04vm>
2016-10-28 17:02:32 +0800

    updated RegisterAndNotify

--- cpp_observer.h
+++ cpp_observer.h
@@ -1,6 +1,17 @@
 #ifndef __CPP_OBSERVER__
 #define __CPP_OBSERVER__
+#include <map>
 
+template <typename Event>
+class Observers {
+using ObsFunc = std::function<void(const Event&)>;
+public:
+	void registerObserver (ObsFunc func){
+		_observers.push_back (func);
+	}
+private:
+	std::vector<ObsFunc> _observers;
 
+};
 
 #endif //__CPP_OBSERVER__
--- tests.cpp
+++ tests.cpp
@@ -1,6 +1,39 @@
 #include "gmock/gmock.h"
 #include "cpp_observer.h"
 
+using namespace std;
+
+class TestEvent {
+	string msg;
+};
+
+struct Callback {
+	void operator() (const TestEvent& event) const {
+	}
+};
+TEST(CPP_OBSERVER, registerAndNotify ) {
+	// TODO: registered observer should receive notification
+	Observers<TestEvent> eventObservers;
+
+
+	eventObservers.registerObserver (Callback());
+
+}
+
+TEST(CPP_OBSERVER, multipleRegisterAndNotify ) {
+	// TODO: multiple registered observer should receive notification
+
+	// TODO: at least 1 normal function and 1 lambda
+}
+
+TEST(CPP_OBSERVER, registerAndDeregister ) {
+	// TODO: register and then deregister -> should not receive any notify
+}
+
+TEST(CPP_OBSERVER, registeredObserverIsFreedWithoutDeregister ) {
+	// TODO: a registered observed is freed and not deregistered
+}
+
 int main(int argc, char *argv[])
 {
 	testing::InitGoogleMock (&argc, argv);
