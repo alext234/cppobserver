20b11521acbbac739a44ee3043365e360fcbd81a
alex <u@ubuntu-14.04vm>
2016-11-01 14:16:08 +0800

    able to register and get notified

--- cpp_observer.h
+++ cpp_observer.h
@@ -3,15 +3,29 @@
 #include <map>
 
 template <typename Event>
-class Observers {
-using ObsFunc = std::function<void(const Event&)>;
+class AbstractObserver {
 public:
-	void registerObserver (ObsFunc func){
-		_observers.push_back (func);
-	}
-private:
-	std::vector<ObsFunc> _observers;
+	virtual void onNotified (const Event& event) = 0;
+};
 
+
+template <typename Event>
+class ObservedSubject{
+public:
+	void registerObserver (AbstractObserver<Event>* ob){
+		_observers.push_back (ob);
+	}
+protected: 
+	void notifyObservers(const Event& event){
+		for (auto it = _observers.begin(); it !=_observers.end (); ++it) {
+			(*it)->onNotified (event);
+			
+		}
+
+	}
+
+private:
+	std::vector<AbstractObserver<Event>*>  _observers;
 };
 
 #endif //__CPP_OBSERVER__
--- tests.cpp
+++ tests.cpp
@@ -1,25 +1,101 @@
 #include "gmock/gmock.h"
 #include "cpp_observer.h"
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
+using namespace testing;
 
-class TestEvent {
-	string msg;
+struct TestEvent {
+	TestEvent (int eventId) : _eventId{eventId} {}
+	int _eventId;
 };
 
-struct Callback {
-	void operator() (const TestEvent& event) const {
+class Subject: public ObservedSubject<TestEvent> {
+public:
+	void notify (const TestEvent & event) { 
+		notifyObservers(event);
 	}
 };
+
+class Observer: public AbstractObserver<TestEvent>{
+public: 
+	Observer() : _receivedEventId{0} {}
+	void onNotified(const TestEvent& event) override {
+		_receivedEventId = event._eventId;
+	}
+	int _receivedEventId;
+};
+
 TEST(CPP_OBSERVER, registerAndNotify ) {
-	// TODO: registered observer should receive notification
-	Observers<TestEvent> eventObservers;
+	Subject subject;
+	Observer observer;
+	subject.registerObserver (&observer);
+	std::srand(std::time(0));
+	const int TEST_EVENT_ID = std::rand();
+	TestEvent event{TEST_EVENT_ID};
+	subject.notify(event);
+	EXPECT_THAT (observer._receivedEventId, Eq(TEST_EVENT_ID));
 
-
-	eventObservers.registerObserver (Callback());
-
+	
 }
+struct TestEvent1 {
+	TestEvent1(int eventId) : _eventId{eventId} {}
+	int _eventId;
+};
+struct TestEvent2 {
+	TestEvent2 (int eventId) : _eventId{eventId} {}
+	int _eventId;
+};
 
+class Subject1: public ObservedSubject<TestEvent1> {
+public:
+	void notify (const TestEvent1 & event) { 
+		notifyObservers(event);
+	}
+};
+
+class Subject2: public ObservedSubject<TestEvent2> {
+public:
+	void notify (const TestEvent2 & event) { 
+		notifyObservers(event);
+	}
+
+};
+
+class ObserverMulti: public AbstractObserver<TestEvent1>, public AbstractObserver<TestEvent2> {
+public:
+	ObserverMulti() : _receivedEventId1{0}, _receivedEventId2{0} {}
+	void onNotified(const TestEvent1& event) override {
+		_receivedEventId1	 = event._eventId;
+	}
+	void onNotified(const TestEvent2& event) override {
+		_receivedEventId2=event._eventId;
+	}
+	int _receivedEventId1, _receivedEventId2;
+};
+
+TEST(CPP_OBSERVER, registerAndNotifyMultipleEvent ) {
+	Subject1 subject1;
+	Subject2 subject2;
+	ObserverMulti observer;
+	subject1.registerObserver (&observer);
+	subject2.registerObserver (&observer);
+
+	std::srand(std::time(0));
+	const int TEST_EVENT_ID1 = std::rand();
+	const int TEST_EVENT_ID2 = std::rand();
+
+	TestEvent1 event1{TEST_EVENT_ID1};
+	TestEvent2 event2{TEST_EVENT_ID2};
+
+	subject1.notify(event1);
+	subject2.notify(event2);
+	EXPECT_THAT (observer._receivedEventId1, Eq(TEST_EVENT_ID1));
+	EXPECT_THAT (observer._receivedEventId2, Eq(TEST_EVENT_ID2));
+
+	
+}
 TEST(CPP_OBSERVER, multipleRegisterAndNotify ) {
 	// TODO: multiple registered observer should receive notification
 
