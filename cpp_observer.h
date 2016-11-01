48d9affab8442a6bed8301c4a5002a563a8d35bb
alex <u@ubuntu-14.04vm>
2016-11-01 16:15:51 +0800

    clean up to reduce verbosity

--- cpp_observer.h
+++ cpp_observer.h
@@ -1,20 +1,23 @@
 #ifndef __CPP_OBSERVER__
 #define __CPP_OBSERVER__
+#include <memory>
 
 template <typename Event>
 class AbstractObserver {
 public:
 	virtual void onNotified (const Event& event) = 0;
+	virtual ~AbstractObserver() {}
 };
 
 
 template <typename Event>
 class ObservedSubject{
+using ObsEventPtr = AbstractObserver<Event>*;
 public:
-	void registerObserver (AbstractObserver<Event>* ob){
+	void registerObserver (ObsEventPtr ob){
 		_observers.push_back (ob);
 	}
-	void deregisterObserver (AbstractObserver<Event>* ob){
+	void deregisterObserver (ObsEventPtr ob){
 		_observers.erase( std::remove (_observers.begin(), _observers.end(), ob), _observers.end());
 		
 	}
@@ -28,7 +31,7 @@ protected:
 	}
 
 private:
-	std::vector<AbstractObserver<Event>*>  _observers;
+	std::vector<ObsEventPtr>  _observers;
 };
 
 #endif //__CPP_OBSERVER__
